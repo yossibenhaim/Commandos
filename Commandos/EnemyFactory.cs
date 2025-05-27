cd1451c6b7bb037517d52446c47d167ee5980cce
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-27 16:54:55 +0300

    added the calss of EnemyFactory

---
+++ Commandos/EnemyFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class EnemyFactory
+    {
+        public List<Enemy> myEnemy = new List<Enemy>();
+
+        public void addedCommando(string name, string rank)
+        {
+            Enemy enemy = new Enemy(name, rank);
+            myEnemy.Add(enemy);
+
+        }
+    }
+}
