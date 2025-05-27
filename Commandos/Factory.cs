18385d2ea6150c65c81ccb918714397f6693c838
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-27 16:27:30 +0300

    added the calss of commandoFactory

---
+++ Commandos/Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class CommandoFactory
+    {
+        public List<Commando> myCommando = new List<Commando>();
+
+        public void addedCommando(string name, string CodeName, IWeapons Tool)
+        {
+            Commando commando = new Commando(name,CodeName,Tool);
+            myCommando.Add(commando);
+            
+        }
+    }
+}
